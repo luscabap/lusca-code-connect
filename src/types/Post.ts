a9570b930e619294493e0d38b3b620018dc4d39d
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-06-13 12:35:42 -0300

    feat: adding json-server posts.json and making req

---
+++ src/types/Post.ts
@@ -0,0 +1,11 @@
+import { AuthorProps } from "./AuthorProps"
+
+export type Post = {
+  id: number,
+  cover: string,
+  title: string,
+  slug: string,
+  body: string,
+  markdown: string,
+  author: AuthorProps
+}

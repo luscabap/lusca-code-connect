e9c66f99d643ca7d3ac8f257b453cfadae0c475c
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-06-14 12:45:56 -0300

    feat: listing 6 cards per page, adding pagination and styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,19 +3,38 @@ import { Pesquisa } from "@/components/Pesquisa";
 import logger from "@/logger";
 import { Post } from "@/types/Post";
 import styles from "./page.module.css"
+import Link from "next/link";
 
-async function getAllPosts(): Promise<Post[]> {
-  const response = await fetch("http://localhost:3042/posts");
+interface IResponseProps {
+  data: Post[],
+  prev: number | null,
+  next: number | null
+}
+
+type teste = {
+  page: string,
+  teste: string
+}
+
+interface IHomePageProps {
+  searchParams: string | undefined | {
+    page: string
+  }
+}
+
+async function getAllPosts(page: number): Promise<IResponseProps> {
+  const response = await fetch(`http://localhost:3042/posts?_page=${page}&_per_page=6`);
   if(!response.ok){
     logger.error("Ops, algo deu errado");
-    return []
+    return { data: [], prev: null, next: null }
   }
   logger.info("Posts obtidos com sucesso!")
   return response.json();
 }
 
-export default async function Home() {
-  const posts = await getAllPosts();
+export default async function Home({ searchParams }: IHomePageProps) {
+  const currentPage = searchParams?.page || 1;
+  const { data: posts, prev, next } = await getAllPosts(currentPage);
   return (
     <main className={styles.container}>
       <Pesquisa textoBotao="Buscar" textoInput="Digite o que você procura"/>
@@ -30,7 +49,10 @@ export default async function Home() {
           author={post.author}
         />) }
       </div>
-      
+      <div className={styles.containerLinks}>
+        {prev && <Link href={`/?page=${prev}`} className={styles.linkPagina}>Página anterior</Link>}
+        {next && <Link href={`/?page=${next}`} className={styles.linkPagina}>Próxima página</Link>}
+      </div>
     </main>
   );
 }
--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,11 +1,14 @@
+"use client"
+import { useRouter } from "next/navigation";
 import styles from "./aside.module.css";
 import logo from "./logo.png";
 import Image from "next/image";
 
 export const Aside = () => {
+  const navigate = useRouter();
   return (
     <aside className={styles.container}>
-      <Image src={logo} alt="Logo do Code Connect" width={128} height={40}/>
+      <Image src={logo} alt="Logo do Code Connect" width={128} height={40} onClick={() => navigate.push("/")} className={styles.logo}/>
     </aside>
   )
 }

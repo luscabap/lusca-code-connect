8e0e0ef4cd778e5e3425c516fccda715ff9584b9
luscabap <lucasbaptistasilva.dev@gmail.com>
2024-07-03 19:53:00 -0300

    feat: new server action (post comment)

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -21,3 +21,22 @@ export async function incrementThumbsUp(post: Post){
   revalidatePath("/")
   revalidatePath(`/${post.slug}`)
 }
+
+export async function postComment(post: Post, formData: HTMLFormElement){
+  const author = await db.user.findFirst({
+    where: {
+      username: 'anabeatriz_dev'
+    }
+  })
+
+  await db.comment.create({
+    data: {
+      text: formData.get('text'),
+      authorId: author!.id,
+      postId: post.id
+    }
+  })
+
+  revalidatePath("/")
+  revalidatePath(`/${post.slug}`)
+}
